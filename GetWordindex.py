90cfcb53a108a2561e8aac74a99a291deff31d2f
kuangxiong <kuangxiong@lsec.cc.ac.cn>
2018-12-26 16:19:53 +0800

    get the word index in vocabulary

---
+++ GetWordindex.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+'''
+---------------------------------------------------------
+ File Name: GetWordindex.py
+ Author:kuangxiong
+ Mail: kuangxiong@lsec.cc.ac.cn
+ Created Time: 2018年12月26日 星期三 16时03分26秒
+---------------------------------------------------------
+'''
+import codecs 
+import sys
+
+RAW_DATA="simple-examples/data/ptb.train.txt"
+VOCAB = "ptb.vocab"
+OUTPUT_DATA = 'ptb.train'
+
+with codecs.open(VOCAB, "r", "utf-8") as f_vocab:
+    vocab = [w.strip() for w in f_vocab.readlines()]
+
+word_to_id={k:v for (k, v) in zip(vocab, range(len(vocab)))}
+
+def get_id(word):
+    return word_to_id[word] if word in word_to_id else word_to_id["<unk>"]
+
+fin = codecs.open(RAW_DATA, "r", "utf-8")
+fout = codecs.open(OUTPUT_DATA, 'w', 'utf-8')
+
+for line in fin:
+    words = line.strip().split()+["<eos>"]
+    out_line=' '.join([str(get_id(w)) for w in words]) +'\n'
+    fout.write(out_line)
+fin.close()
+fout.close()
+
