6e84b89d61abfe0f00137baf9688666443fd3af6
kuangxiong <kuangxiong@lsec.cc.ac.cn>
2018-12-26 22:55:31 +0800

    add wort_to_index

--- DataPreprocess.py
+++ DataPreprocess.py
@@ -11,6 +11,8 @@
 import codecs
 import collections
 from operator import itemgetter
+import codecs 
+import sys
 
 RAW_DATA = "simple-examples/data/ptb.train.txt"
 VOCAB_OUTPUT = 'ptb.vocab'
@@ -30,6 +32,35 @@ sorted_words=["<eos>"] + sorted_words
 with codecs.open(VOCAB_OUTPUT, 'w', 'utf-8') as file_output:
     for word in sorted_words:
         file_output.write(word+'\n')
+
+
+
+RAW_DATA="simple-examples/data/ptb.train.txt"
+VOCAB = "ptb.vocab"
+OUTPUT_DATA = 'ptb.train'
+
+with codecs.open(VOCAB, "r", "utf-8") as f_vocab:
+    vocab = [w.strip() for w in f_vocab.readlines()]
+
+word_to_id={k:v for (k, v) in zip(vocab, range(len(vocab)))}
+
+def get_id(word):
+    return word_to_id[word] if word in word_to_id else word_to_id["<unk>"]
+
+fin = codecs.open(RAW_DATA, "r", "utf-8")
+fout = codecs.open(OUTPUT_DATA, 'w', 'utf-8')
+
+for line in fin:
+    words = line.strip().split()+["<eos>"]
+    out_line=' '.join([str(get_id(w)) for w in words]) +'\n'
+    fout.write(out_line)
+fin.close()
+fout.close()
+
+
+
+
+
 
 
 #TRAIN_DATA = "ptb.train"
